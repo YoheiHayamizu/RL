02a19084447e46d65361223f9bf2df0ae6787eb9
YoheiHayamizu <yayuyo_you@icloud.com>
2019-05-01 12:33:17 -0400

    feat: Qlearning and Sarsa

---
+++ methods.py
@@ -0,0 +1,65 @@
+import numpy as np
+import space
+
+
+class QLearning:
+    def __init__(self, dim_state, dim_action, alpha=0.5, gamma=0.99):
+        self.num_state = space.NUM_DIGITIZED ** dim_state
+        self.num_action = dim_action
+        self.alpha = alpha
+        self.gamma = gamma
+        self.Q = np.zeros((self.num_state, self.num_action))
+
+    def update(self, observation, action, reward, next_observation):
+        state = space.digitize_state(observation)
+        next_state = space.digitize_state(next_observation)
+        next_action = self.get_max_action(next_observation)
+        self.Q[state][action] += self.alpha * (
+                reward + self.gamma * self.Q[next_state][next_action] - self.Q[state][action])
+
+    def get_max_action(self, observation):
+        state = space.digitize_state(observation)
+        action, = np.where(self.Q[state] == np.max(self.Q[state]))
+        return np.random.choice(action)
+
+    def epsilon_greedy(self, observation, t):
+        epsilon = np.exp(-t/10)
+        if np.random.random() >= epsilon:
+            action = self.get_max_action(observation)
+        else:
+            action = np.random.randint(0, 2)
+        return action
+
+    def greedy(self, observation):
+        return self.get_max_action(observation)
+
+
+class Sarsa:
+    def __init__(self, dim_state, dim_action, alpha=0.5, gamma=0.99):
+        self.num_state = space.NUM_DIGITIZED ** dim_state
+        self.num_action = dim_action
+        self.alpha = alpha
+        self.gamma = gamma
+        self.Q = np.zeros((self.num_state, self.num_action))
+
+    def update(self, observation, action, reward, next_observation, next_action):
+        state = space.digitize_state(observation)
+        next_state = space.digitize_state(next_observation)
+        self.Q[state][action] += self.alpha * (
+                reward + self.gamma * self.Q[next_state][next_action] - self.Q[state][action])
+
+    def get_max_action(self, observation):
+        state = space.digitize_state(observation)
+        action, = np.where(self.Q[state] == np.max(self.Q[state]))
+        return np.random.choice(action)
+
+    def epsilon_greedy(self, observation, t):
+        epsilon = np.exp(-t/10)
+        if np.random.random() >= epsilon:
+            action = self.get_max_action(observation)
+        else:
+            action = np.random.randint(0, 2)
+        return action
+
+    def greedy(self, observation):
+        return self.get_max_action(observation)
---
+++ test_cartpole.py
@@ -0,0 +1,126 @@
+import gym
+import methods
+import cv2
+import matplotlib.pyplot as plt
+import csv
+
+EPISODE = 2000
+TIMESTEP = 200
+RECORD_EPS = [100, 500, 1000, 2000]
+MOVIE_DIR = "./movies/"
+IMG_DIR = "./images/"
+FIG_DIR = "./figures/"
+
+
+def display_frames_as_gif(frames, i_episode, _dir):
+    framelist = list()
+    imgsize_x = frames[0].shape[1]
+    imgsize_y = frames[0].shape[0]
+    for i, frame in enumerate(frames):
+        framename = IMG_DIR + _dir + 'cartpole_{0:04d}_{1:03d}.png'.format(i_episode + 1, i + 1)
+        cv2.imwrite(framename, frame)
+        framelist.append(framename)
+
+    # 動画作成
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video = cv2.VideoWriter(MOVIE_DIR + _dir + 'cartpole_{0:04d}.mp4'.format(i_episode + 1),
+                            fourcc, 20.0, (imgsize_x, imgsize_y))
+
+    for img_file in framelist:
+        img = cv2.imread(img_file)
+        video.write(img)
+    video.release()
+    del fourcc
+    del video
+
+
+def discrete_qlearning():
+    env = gym.make('CartPole-v0')
+    agent = methods.QLearning(4, 2)
+    timestep_list = list()
+
+    for episode in range(EPISODE):
+        observation = env.reset()
+        action = env.action_space.sample()
+        frames = list()
+        for t in range(TIMESTEP):
+            if episode + 1 in RECORD_EPS:
+                frames.append(env.render(mode='rgb_array'))
+
+            pre_observation = observation
+            pre_action = action
+            observation, reward, done, info = env.step(pre_action)
+            action = agent.epsilon_greedy(observation, episode)
+            if done:
+                print("Episode finished after {} timesteps".format(t + 1))
+                # average_step = average_step[1:] + [t]
+                if t < TIMESTEP * 0.95:
+                    reward = -0.1
+                else:
+                    reward = 1
+                agent.update(pre_observation, pre_action, reward, observation)
+                break
+            else:
+                reward = 0
+                agent.update(pre_observation, pre_action, reward, observation)
+
+        timestep_list.append(t)
+        if episode + 1 in RECORD_EPS:
+            display_frames_as_gif(frames, episode, "QLearning/")
+
+    fig, ax = plt.subplots()
+    ax.plot(range(len(timestep_list)), timestep_list)
+    ax.set_xlabel("Episode")
+    ax.set_ylabel("Step")
+    ax.set_title("QLearning")
+    plt.savefig(FIG_DIR + "QLearning.png")
+    env.close()
+    return timestep_list
+
+
+def discrete_sarsa():
+    env = gym.make('CartPole-v0')
+    agent = methods.Sarsa(4, 2)
+    timestep_list = list()
+
+    for episode in range(EPISODE):
+        observation = env.reset()
+        action = env.action_space.sample()
+        frames = list()
+        for t in range(TIMESTEP):
+            if episode + 1 in RECORD_EPS:
+                frames.append(env.render(mode='rgb_array'))
+
+            pre_observation = observation
+            pre_action = action
+            observation, reward, done, info = env.step(pre_action)
+            action = agent.get_max_action(observation)
+            if done:
+                print("Episode finished after {} timesteps".format(t + 1))
+                # average_step = average_step[1:] + [t]
+                if t < TIMESTEP * 0.95:
+                    reward = -0.1
+                else:
+                    reward = 1
+                agent.update(pre_observation, pre_action, reward, observation, action)
+                break
+            else:
+                reward = 0
+                agent.update(pre_observation, pre_action, reward, observation, action)
+
+        timestep_list.append(t)
+        if episode + 1 in RECORD_EPS:
+            display_frames_as_gif(frames, episode, "Sarsa/")
+
+    fig, ax = plt.subplots()
+    ax.plot(range(len(timestep_list)), timestep_list)
+    ax.set_xlabel("Episode")
+    ax.set_ylabel("Step")
+    ax.set_title("Sarsa")
+    plt.savefig(FIG_DIR + "Sarsa.png")
+    env.close()
+    return timestep_list
+
+
+discrete_qlearning()
+discrete_sarsa()
