8daa6b8d77de9cedec9de57f937a77293177993e
YoheiHayamizu <yayuyo_you@icloud.com>
2020-05-28 07:08:56 +0900

    chore: update package

--- run.py
+++ run.py
@@ -1,16 +1,10 @@
 import numpy as np
 import pandas as pd
-import dill
 import copy
 import seaborn as sns;
 import optparse
-import pathlib
 import sys
-
-current_dir = pathlib.Path(__file__).resolve().parent
-sys.path.append(str(current_dir) + '/../')
-
-from config import *
+from .config import *
 
 sns.set()
 
@@ -141,12 +135,12 @@ def parseOptions():
 
 
 if __name__ == "__main__":
-    from mdp.MDPGridworld import MDPGridWorld
+    from .mdp.MDPGridworld import MDPGridWorld
 
-    from agent.qlearning import QLearningAgent
-    from agent.sarsa import SarsaAgent
-    from agent.rmax import RMAXAgent
-    from agent.dynaq import DynaQAgent
+    from .agent.qlearning import QLearningAgent
+    from .agent.sarsa import SarsaAgent
+    from .agent.rmax import RMAXAgent
+    from .agent.dynaq import DynaQAgent
 
     opts = parseOptions()
 
@@ -174,7 +168,7 @@ if __name__ == "__main__":
     ###########################
     # GET THE DISPLAY ADAPTER
     ###########################
-    from mdp import Display as graphic
+    from .mdp import Display as graphic
 
     display = graphic.GraphicsGridworldDisplay(mdp)
     try:
