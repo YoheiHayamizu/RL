9164184784786049f991d32533a6b372d6f4d649
YoheiHayamizu <yayuyo_you@icloud.com>
2020-09-08 10:23:05 +0900

    refactor: update MDPBasicsClass

--- mdp/MDPBasis.py
+++ mdp/MDPBasis.py
@@ -1,35 +1,38 @@
 import copy
 import dill
+from typing import Union, List, Any, Optional, Dict
 
 
 class MDPBasisClass(object):
     """ abstract class for a MDP """
-
-    def __init__(self, init_state, actions, transition_func, reward_func, step_cost):
+    def __init__(self,
+                 init_state: Any,
+                 transition_func: Any,
+                 reward_func: Any,
+                 actions: Any = None):
         self.init_state = copy.deepcopy(init_state)
-        self.cur_state = self.init_state
+        self.current_state = self.init_state
         self.actions = actions
         self.__transition_func = transition_func
         self.__reward_func = reward_func
-        self.step_cost = step_cost
 
     # Accessors
 
-    def get_params(self):
+    def get_params(self) -> dict:
         """
         Returns:
             <dict> key -> param_name, val -> param_value
         """
         param_dict = dict()
-        param_dict["step_cost"] = self.step_cost
-
+        param_dict["init_state"] = self.init_state
+        param_dict["actions"] = self.actions
         return param_dict
 
     def get_init_state(self):
         return self.init_state
 
-    def get_cur_state(self):
-        return self.cur_state
+    def get_current_state(self):
+        return self.current_state
 
     def get_actions(self, state):
         return self.actions
@@ -39,9 +42,6 @@ class MDPBasisClass(object):
 
     def get_reward_func(self):
         return self.__reward_func
-
-    def get_step_cost(self):
-        return self.step_cost
 
     # Setters
 
@@ -54,9 +54,6 @@ class MDPBasisClass(object):
     def set_transition_func(self, new_transition_func):
         self.__transition_func = new_transition_func
 
-    def set_step_cost(self, new_step_cost):
-        self.step_cost = new_step_cost
-
     # Core
 
     def step(self, action):
@@ -67,16 +64,16 @@ class MDPBasisClass(object):
                  done: <bool>,
                  info: <dict>
         """
-        next_state = self.__transition_func(self.cur_state, action)
-        reward = self.__reward_func(self.cur_state, action, next_state)
-        done = self.cur_state.is_terminal()
-        self.cur_state = next_state
+        next_state = self.__transition_func(self.current_state, action)
+        reward = self.__reward_func(self.current_state, action, next_state)
+        done = self.current_state.is_terminal()
+        self.current_state = next_state
 
         return next_state, reward, done, self.get_params()
 
     def reset(self):
-        self.cur_state = self.init_state
-        return self.cur_state
+        self.current_state = self.init_state
+        return self.current_state
 
     def to_pickle(self, filename):
         with open(filename, "wb") as f:
